c10e9457218a697e41e417264f76f7b65ab54b18
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-04-26 23:51:41 +0200

    got rid of useCallback import

--- src/components/LogInForm.jsx
+++ src/components/LogInForm.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from "react"
+import React, { useEffect } from "react"
 import { useState } from "react"
 import { makeStyles } from "tss-react/mui"
 import { motion } from "framer-motion"
