77e01791f8dcc3230c37829b4586efbb9de39df4
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-06-01 00:55:26 +0200

    created AppSettingsMenu component

---
+++ src/components/AppSettingsMenu.jsx
@@ -0,0 +1,37 @@
+/* Libraries imports */
+import React, { useState, useEffect } from "react"
+import { makeStyles } from "tss-react/mui"
+import { motion } from "framer-motion"
+import { useNavigate } from "react-router"
+/* Components imports */
+
+/* React Icons imports */
+
+/* Helper functions imports */
+
+
+
+
+const useStyles = makeStyles()((theme) => {
+	return {
+		root: {
+			zIndex: 6,
+		},
+	}
+})
+
+
+
+const AppSettingsMenu = ({ setIsLoading }) => {
+	const { classes } = useStyles()
+	const navigate = useNavigate()
+
+
+	return  <div className={ classes.root }>
+				
+			</div>
+}
+
+
+
+export default AppSettingsMenu
