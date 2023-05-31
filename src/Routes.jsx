2d4e29de2890416a6392e2eb44282c57c327afac
Florian Douay <79285331+Altervayne@users.noreply.github.com>
2023-06-01 00:58:57 +0200

    added TermsOfUse to react routing logic

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import DashBoard from "./pages/DashBoard"
 import PageNotFound from "./pages/PageNotFound"
 import ChatRoom from "./pages/ChatRoom"
+import TermsOfUse from "./pages/TermsOfUse"
 
 const ReactRoutes = () => {
 	return (
@@ -12,6 +13,7 @@ const ReactRoutes = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='/dashboard' element={<DashBoard />} />
 				<Route path='/room/:id' element={<ChatRoom />} />
+				<Route path='/termsofuse' element={<TermsOfUse />} />
 				<Route path='*' element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
